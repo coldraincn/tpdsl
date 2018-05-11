ce54a12e91bbde94af11fac9467e41da20784991
coldraincn <handan213@gmail.com>
2018-05-11 15:03:33 +0800

    LL(K)

---
+++ src/main/java/com/coldraincn/parsing/multi/Test.java
@@ -0,0 +1,8 @@
+package com.coldraincn.parsing.multi;
+public class Test {
+    public static void main(String[] args) {
+        LookaheadLexer lexer = new LookaheadLexer(args[0]); // parse arg
+        LookaheadParser parser = new LookaheadParser(lexer, 2);
+        parser.list(); // begin parsing at rule list
+    }
+}
